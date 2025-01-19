c3395ed76fea89e43755a875b54b9a8d04ae1bb6
sangeun-b <baek0007@algonquinlive.com>
2025-01-19 21:26:48 +0900

    Feat: 회원 관련 enum 생성

---
+++ backend/src/main/java/com/ourdressingtable/domain/member/domain/AuthType.java
@@ -0,0 +1,20 @@
+package com.ourdressingtable.domain.member.domain;
+
+import lombok.Getter;
+
+@Getter
+public enum AuthType {
+    KAKAO(0, "KAKAO"),
+    NAVER(1, "NAVER"),
+    GOOGLE(2, "GOOGLE"),
+    APPLE(3, "APPLE"),
+    GITHUB(4, "GITHUB");
+
+    private final int code;
+    private final String name;
+
+    AuthType(int code, String name) {
+        this.code = code;
+        this.name = name;
+    }
+}
---
+++ backend/src/main/java/com/ourdressingtable/domain/member/domain/Status.java
@@ -0,0 +1,24 @@
+package com.ourdressingtable.domain.member.domain;
+
+import lombok.Getter;
+
+@Getter
+public enum Status {
+    ACTIVATE(0, "활성화", "활성화 중인 계정"),
+    LOCK(1, "일시 중지", "일시 중지 중인 계정"),
+    BLOCK(2, "영구 중지", "영구 중지 중인 계정");
+
+    private final int code;
+    private final String name;
+    private final String description;
+
+    Status(final int code, final String name, final String description) {
+        this.code = code;
+        this.name = name;
+        this.description = description;
+    }
+
+
+}
+
+
