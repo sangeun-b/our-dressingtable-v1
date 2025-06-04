577ebe339779045080a39189a0cc4c485e98f362
stella317 <stella.se.baek@gmail.com>
2025-06-04 15:17:55 +0900

    refactor: hash, masking util에 null 체크 추가

--- backend/src/main/java/com/ourdressingtable/common/util/MaskingUtil.java
+++ backend/src/main/java/com/ourdressingtable/common/util/MaskingUtil.java
@@ -1,15 +1,29 @@
-package com.ourdressingtable.util;
+package com.ourdressingtable.common.util;
+
+import com.ourdressingtable.common.exception.OurDressingTableException;
+import org.apache.commons.lang3.StringUtils;
 
 public class MaskingUtil {
 
     public static String maskedEmail(String email) {
+        if(StringUtils.isBlank(email))
+            throw new IllegalArgumentException("Email cannot be null or empty");
+
         String[] parts = email.split("@");
+
+        if(parts.length != 2 || parts[0].isEmpty() || parts[1].isEmpty())
+            throw new IllegalArgumentException("Invalid email format");
+
         String namePart = parts[0];
         String domainPart = parts[1];
 
         String maskedName = namePart.charAt(0) + "." + namePart.charAt(0) + "***";
 
         String[] domainParts = domainPart.split("\\.");
+
+        if(domainParts.length != 2 || domainParts[0].isEmpty() || domainParts[1].isEmpty())
+            throw new IllegalArgumentException("Invalid domain format");
+
         String maskedDomain = domainParts[0].charAt(0)+"*****";
 
         return maskedName + "@" + maskedDomain + "." + domainParts[1];
@@ -17,6 +31,12 @@ public class MaskingUtil {
     }
 
     public static String maskedPhone(String phone) {
+        if(StringUtils.isBlank(phone))
+            throw new IllegalArgumentException("Phone cannot be null or empty");
+
+        if(!phone.matches("\\d{3}-\\d{4}-\\d{4}"))
+            throw new IllegalArgumentException("Phone number format must be xxx-xxxx-xxxx");
+
         return phone.replaceAll("(\\d{3})-\\d{4}-(\\d{4})", "$1****$2");
     }
 
