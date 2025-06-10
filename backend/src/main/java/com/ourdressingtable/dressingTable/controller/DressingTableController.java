5028657fc300395853e5060df3c13706133d43b2
stella317 <stella.se.baek@gmail.com>
2025-06-10 10:29:46 +0900

    refactor: 변경된 로그인 회원 정보 조회 로직 적용

--- backend/src/main/java/com/ourdressingtable/dressingTable/controller/DressingTableController.java
+++ backend/src/main/java/com/ourdressingtable/dressingTable/controller/DressingTableController.java
@@ -1,5 +1,6 @@
 package com.ourdressingtable.dressingTable.controller;
 
+import com.ourdressingtable.common.util.SecurityUtil;
 import com.ourdressingtable.dressingTable.domain.DressingTable;
 import com.ourdressingtable.dressingTable.dto.CreateDressingTableResponse;
 import com.ourdressingtable.dressingTable.dto.DressingTableRequest;
@@ -8,6 +9,8 @@ import com.ourdressingtable.member.domain.Member;
 import com.ourdressingtable.member.service.MemberService;
 import com.ourdressingtable.security.dto.CustomUserDetails;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -27,18 +30,24 @@ import java.net.URI;
 @RequiredArgsConstructor
 @RequestMapping("/api/dressing-tables")
 @Tag(name = "화장대", description = "화장대 관련 API")
+@ApiResponses({
+        @ApiResponse(responseCode = "201", description = "화장대 생성 성공"),
+        @ApiResponse(responseCode = "400", description = "잘못된 요청"),
+        @ApiResponse(responseCode = "403", description = "인증되지 않은 사용자"),
+        @ApiResponse(responseCode = "500", description = "서버 오류")
+})
 public class DressingTableController {
 
     private final DressingTableService dressingTableService;
     private final MemberService memberService;
 
-    @PostMapping("")
+    @PostMapping()
     @Operation(summary = "화장대 생성", description = "새로운 화장대를 생성합니다.")
-    public ResponseEntity<CreateDressingTableResponse> addDressingTable(@RequestBody @Valid DressingTableRequest dressingTableRequest,
-                                                          @AuthenticationPrincipal CustomUserDetails customUserDetails) {
-        Long id = dressingTableService.createDressingTable(dressingTableRequest, customUserDetails.getMemberId());
+    public ResponseEntity<CreateDressingTableResponse> addDressingTable(@RequestBody @Valid DressingTableRequest dressingTableRequest) {
+        Long currentMemberId = SecurityUtil.getCurrentMemberId();
+        Long id = dressingTableService.createDressingTable(dressingTableRequest, currentMemberId);
 
-        URI  location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
                 .buildAndExpand(id).toUri();
 
         return ResponseEntity.created(location)
