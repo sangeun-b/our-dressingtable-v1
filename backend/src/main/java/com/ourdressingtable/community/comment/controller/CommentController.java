d11b508134f046baa6783137876a9753d00b24b9
stella317 <stella.se.baek@gmail.com>
2025-06-11 17:48:39 +0900

    feat: 댓글 삭제 기능 추가

--- backend/src/main/java/com/ourdressingtable/community/comment/controller/CommentController.java
+++ backend/src/main/java/com/ourdressingtable/community/comment/controller/CommentController.java
@@ -1,5 +1,6 @@
 package com.ourdressingtable.community.comment.controller;
 
+import com.ourdressingtable.common.util.SecurityUtil;
 import com.ourdressingtable.community.comment.dto.CreateCommentRequest;
 import com.ourdressingtable.community.comment.dto.CreateCommentResponse;
 import com.ourdressingtable.community.comment.repository.CommentRepository;
@@ -7,6 +8,7 @@ import com.ourdressingtable.community.comment.service.CommentService;
 import com.ourdressingtable.community.post.dto.CreatePostResponse;
 import com.ourdressingtable.security.dto.CustomUserDetails;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import java.net.URI;
@@ -14,10 +16,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -28,12 +27,19 @@ public class CommentController {
 
     private final CommentService commentService;
 
-    @Operation(summary = "댓글 작성", description = "새 댓글을 작성합니다.")
+    @Operation(summary = "댓글 작성", description = "새 댓글을 작성합니다.", security = @SecurityRequirement(name = "bearerAuth"))
     @PostMapping()
     public ResponseEntity<CreateCommentResponse> createComment(@RequestBody @Valid CreateCommentRequest request) {
         Long commentId = commentService.createComment(request);
         return ResponseEntity.created(URI.create("/api/comments/" + commentId)).body(
                 CreateCommentResponse.builder().id(commentId).build());
     }
+
+    @Operation(summary = "댓글 삭제", description = "댓글을 삭제합니다.", security = @SecurityRequirement(name = "bearerAuth"))
+    public ResponseEntity deleteComment(@PathVariable("commentId") Long commentId) {
+        commentService.deleteComment(commentId);
+        return ResponseEntity.noContent().build();
+    }
+
 
 }
