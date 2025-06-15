f0b4b798080eb6ea74eda29d316f481a09ebfdb4
sangeun-b <baek0007@algonquinlive.com>
2025-06-15 15:58:56 +0900

    feat: querydsl class 추가

---
+++ backend/src/main/generated/com/ourdressingtable/cosmetic/domain/QCosmetic.java
@@ -0,0 +1,88 @@
+package com.ourdressingtable.cosmetic.domain;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QCosmetic is a Querydsl query type for Cosmetic
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QCosmetic extends EntityPathBase<Cosmetic> {
+
+    private static final long serialVersionUID = -1513138940L;
+
+    private static final PathInits INITS = PathInits.DIRECT2;
+
+    public static final QCosmetic cosmetic = new QCosmetic("cosmetic");
+
+    public final StringPath allergens = createString("allergens");
+
+    public final StringPath barcode = createString("barcode");
+
+    public final QCosmeticBrand brand;
+
+    public final StringPath certifications = createString("certifications");
+
+    public final QCosmeticCategory cosmeticCategory;
+
+    public final StringPath countryOfOrigin = createString("countryOfOrigin");
+
+    public final StringPath description = createString("description");
+
+    public final StringPath expirationType = createString("expirationType");
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final StringPath imageUrl = createString("imageUrl");
+
+    public final StringPath ingredients = createString("ingredients");
+
+    public final StringPath manufacturer = createString("manufacturer");
+
+    public final StringPath name = createString("name");
+
+    public final StringPath officialUrl = createString("officialUrl");
+
+    public final StringPath productType = createString("productType");
+
+    public final StringPath releasedAt = createString("releasedAt");
+
+    public final StringPath scent = createString("scent");
+
+    public final StringPath skinType = createString("skinType");
+
+    public final StringPath texture = createString("texture");
+
+    public final StringPath volume = createString("volume");
+
+    public QCosmetic(String variable) {
+        this(Cosmetic.class, forVariable(variable), INITS);
+    }
+
+    public QCosmetic(Path<? extends Cosmetic> path) {
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
+    }
+
+    public QCosmetic(PathMetadata metadata) {
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QCosmetic(PathMetadata metadata, PathInits inits) {
+        this(Cosmetic.class, metadata, inits);
+    }
+
+    public QCosmetic(Class<? extends Cosmetic> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.brand = inits.isInitialized("brand") ? new QCosmeticBrand(forProperty("brand")) : null;
+        this.cosmeticCategory = inits.isInitialized("cosmeticCategory") ? new QCosmeticCategory(forProperty("cosmeticCategory")) : null;
+    }
+
+}
+
---
+++ backend/src/main/generated/com/ourdressingtable/memberCosmetic/domain/QMemberCosmetic.java
@@ -0,0 +1,71 @@
+package com.ourdressingtable.memberCosmetic.domain;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QMemberCosmetic is a Querydsl query type for MemberCosmetic
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QMemberCosmetic extends EntityPathBase<MemberCosmetic> {
+
+    private static final long serialVersionUID = -623584328L;
+
+    private static final PathInits INITS = PathInits.DIRECT2;
+
+    public static final QMemberCosmetic memberCosmetic = new QMemberCosmetic("memberCosmetic");
+
+    public final com.ourdressingtable.cosmetic.domain.QCosmetic cosmetic;
+
+    public final com.ourdressingtable.dressingTable.domain.QDressingTable dressingTable;
+
+    public final DatePath<java.time.LocalDate> expiredDate = createDate("expiredDate", java.time.LocalDate.class);
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final StringPath imageUrl = createString("imageUrl");
+
+    public final com.ourdressingtable.member.domain.QMember member;
+
+    public final DatePath<java.time.LocalDate> openDate = createDate("openDate", java.time.LocalDate.class);
+
+    public final NumberPath<java.math.BigDecimal> price = createNumber("price", java.math.BigDecimal.class);
+
+    public final BooleanPath setNotification = createBoolean("setNotification");
+
+    public final StringPath store = createString("store");
+
+    public final DatePath<java.time.LocalDate> useByDate = createDate("useByDate", java.time.LocalDate.class);
+
+    public QMemberCosmetic(String variable) {
+        this(MemberCosmetic.class, forVariable(variable), INITS);
+    }
+
+    public QMemberCosmetic(Path<? extends MemberCosmetic> path) {
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
+    }
+
+    public QMemberCosmetic(PathMetadata metadata) {
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QMemberCosmetic(PathMetadata metadata, PathInits inits) {
+        this(MemberCosmetic.class, metadata, inits);
+    }
+
+    public QMemberCosmetic(Class<? extends MemberCosmetic> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.cosmetic = inits.isInitialized("cosmetic") ? new com.ourdressingtable.cosmetic.domain.QCosmetic(forProperty("cosmetic"), inits.get("cosmetic")) : null;
+        this.dressingTable = inits.isInitialized("dressingTable") ? new com.ourdressingtable.dressingTable.domain.QDressingTable(forProperty("dressingTable"), inits.get("dressingTable")) : null;
+        this.member = inits.isInitialized("member") ? new com.ourdressingtable.member.domain.QMember(forProperty("member")) : null;
+    }
+
+}
+
