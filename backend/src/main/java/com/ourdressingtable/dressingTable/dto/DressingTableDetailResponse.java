2ae6b5509cf24f96986b3801957c196bfc85f573
sangeun-b <baek0007@algonquinlive.com>
2025-06-15 15:58:37 +0900

    feat: 화장품 상세 조회 dto 추가

---
+++ backend/src/main/java/com/ourdressingtable/dressingTable/dto/DressingTableDetailResponse.java
@@ -0,0 +1,49 @@
+package com.ourdressingtable.dressingTable.dto;
+
+import com.ourdressingtable.dressingTable.domain.DressingTable;
+import com.ourdressingtable.memberCosmetic.domain.MemberCosmetic;
+import io.swagger.v3.oas.annotations.media.Schema;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class DressingTableDetailResponse {
+
+    @Schema(description = "화장대 id", example = "1")
+    private Long id;
+
+    @Schema(description = "화장대 이름", example = "my dt")
+    private String name;
+
+    @Schema(description = "화장대 이미지", example = "https://image.img")
+    private String imageUrl;
+
+    @Schema(description = "등록된 화장품")
+    private List<MemberCosmetic> memberCosmetics;
+
+    @Builder
+    public DressingTableDetailResponse(Long id, String name, String imageUrl, List<MemberCosmetic> memberCosmetics) {
+        this.id = id;
+        this.name = name;
+        this.imageUrl = imageUrl;
+        this.memberCosmetics = memberCosmetics;
+    }
+
+
+    public static DressingTableDetailResponse from(DressingTable dressingTable) {
+        return DressingTableDetailResponse.builder()
+                .id(dressingTable.getId())
+                .name(dressingTable.getName())
+                .imageUrl(dressingTable.getImageUrl())
+                .memberCosmetics(dressingTable.getMemberCosmetics())
+                .build();
+    }
+}
