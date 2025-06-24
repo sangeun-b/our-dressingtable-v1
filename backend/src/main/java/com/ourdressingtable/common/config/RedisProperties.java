f372a79c2aaca0f3b75dc87c6ad84164be6c97e7
stella317 <stella.se.baek@gmail.com>
2025-06-24 17:58:38 +0900

    fix: RedisProperties에 value 등록

--- backend/src/main/java/com/ourdressingtable/common/config/RedisProperties.java
+++ backend/src/main/java/com/ourdressingtable/common/config/RedisProperties.java
@@ -2,6 +2,7 @@ package com.ourdressingtable.common.config;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,6 +12,9 @@ import org.springframework.context.annotation.Configuration;
 @ConfigurationProperties(prefix = "redis")
 public class RedisProperties {
 
+    @Value("${spring.data.redis.host}")
     private String host;
+
+    @Value("${spring.data.redis.port}")
     private int port;
 }
