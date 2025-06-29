aed6a1e0bda734ffb02734b28b3d9caf49f44b53
sangeun-b <baek0007@algonquinlive.com>
2025-06-29 22:05:40 +0900

    feat: websocket 설정 추가

---
+++ backend/src/main/java/com/ourdressingtable/common/config/WebSocketConfig.java
@@ -0,0 +1,26 @@
+package com.ourdressingtable.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws-chat") // websocket 연결 주소
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/sub"); // 구독 주소
+        registry.setApplicationDestinationPrefixes("/pub"); // 메세지 보낼 주소
+    }
+}
+
