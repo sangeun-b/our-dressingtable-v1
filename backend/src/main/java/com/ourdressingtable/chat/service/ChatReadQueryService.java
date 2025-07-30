27c8ea777cd2fc7e9fe1af316ab314ec5c8e0128
sangeun-b <baek0007@algonquinlive.com>
2025-07-31 01:14:32 +0900

    fix: 빈 순환 참조 수정

--- backend/src/main/java/com/ourdressingtable/chat/service/ChatReadQueryService.java
+++ backend/src/main/java/com/ourdressingtable/chat/service/ChatReadQueryService.java
@@ -1,11 +1,9 @@
 package com.ourdressingtable.chat.service;
 
-import com.ourdressingtable.chat.domain.ChatRead;
-
 import java.time.LocalDateTime;
 
-public interface ChatReadService {
-
-    void markAsRead(String chatroomId);
+public interface ChatReadQueryService {
     LocalDateTime getLastReadAt(String chatroomId);
+
+
 }
